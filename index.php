ee6dc7beabc1d1c846321bd708130e0fbf5b4e0a
Kamal Khan <shout@bhittani.com>
2021-07-22 01:28:28 +0500

    Scaffold kk_star_ratings function

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
  */
 
 use function Bhittani\StarRating\functions\dot;
+use function Bhittani\StarRating\functions\to_shortcode;
 
 if (!defined('ABSPATH')) {
     http_response_code(404);
@@ -31,6 +32,7 @@ if (! function_exists('Bhittani\StarRating\functions\dot')) {
     require __DIR__.'/src/functions/dot.php';
 }
 
+/** @param null|string|array $keyOrItems */
 function kksr($keyOrItems = null, $default = null)
 {
     static $config;
@@ -52,6 +54,26 @@ function kksr($keyOrItems = null, $default = null)
     }
 
     return $config;
+}
+
+/** @param null|int|string|array|object|WP_POST $idOrPostOrPayload */
+function kk_star_ratings($idOrPostOrPayload = null): string
+{
+    $payload = [];
+
+    if (is_array($idOrPostOrPayload)) {
+        $payload = $idOrPostOrPayload;
+    }
+
+    if (is_object($idOrPostOrPayload)) {
+        $payload['id'] = $idOrPostOrPayload->ID;
+    }
+
+    if ($idOrPostOrPayload) {
+        $payload['id'] = $idOrPostOrPayload;
+    }
+
+    return do_shortcode(to_shortcode('kk-star-ratings', $payload));
 }
 
 require_once __DIR__.'/src/index.php';
