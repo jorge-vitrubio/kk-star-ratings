ca5ddcbf4291fce315885e64ca9a7345617f9209
Kamal Khan <shout@bhittani.com>
2021-07-29 21:58:53 +0500

    Minimize assets

--- public/js/kk-star-ratings.js
+++ public/js/kk-star-ratings.js
@@ -1,3 +1,8 @@
+/**
+ * kk Star Ratings
+ * @see https://github.com/kamalkhan/kk-star-ratings
+ */
+
 "use strict";
 
 // (function (fn) {
@@ -30,29 +35,31 @@
 // });
 
 jQuery(document).ready(function ($) {
-  function apply ($el, options) {
+  function apply($el, options) {
     var options = options || { isBusy: false };
 
-    function ajax(data, successCallback, errorCallback)
-    {
-      if (options.isBusy || $el.hasClass('kksr-disabled')) {
+    function ajax(data, successCallback, errorCallback) {
+      if (options.isBusy || $el.hasClass("kksr-disabled")) {
         return;
       }
 
       options.isBusy = true;
 
       $.ajax({
-        type: 'POST',
+        type: "POST",
         url: kk_star_ratings.endpoint,
-        data: Object.assign({
-          nonce: kk_star_ratings.nonce,
-          action: kk_star_ratings.action
-        }, data),
+        data: Object.assign(
+          {
+            nonce: kk_star_ratings.nonce,
+            action: kk_star_ratings.action,
+          },
+          data
+        ),
         error: errorCallback,
         success: successCallback,
         complete: function () {
           options.isBusy = false;
-        }
+        },
       });
     }
 
@@ -66,36 +73,40 @@ jQuery(document).ready(function ($) {
       //   best: $('[data-star]', $el).length
       // };
 
-      ajax({
-        rating: $star.data('star'),
-        payload: $el.data('payload')
-      }, function (response, status, xhr) {
-        var $newEl = $(response);
-        $newEl.addClass($el.attr('class'));
-        $el.replaceWith($newEl);
-        destroy();
-        apply($newEl, options);
-      }, function (xhr, status, err) {
-        if (xhr.responseJSON && xhr.responseJSON.error) {
-          console.error(xhr.responseJSON.error);
+      ajax(
+        {
+          rating: $star.data("star"),
+          payload: $el.data("payload"),
+        },
+        function (response, status, xhr) {
+          var $newEl = $(response);
+          $newEl.addClass($el.attr("class"));
+          $el.replaceWith($newEl);
+          destroy();
+          apply($newEl, options);
+        },
+        function (xhr, status, err) {
+          if (xhr.responseJSON && xhr.responseJSON.error) {
+            console.error(xhr.responseJSON.error);
+          }
         }
-      });
+      );
     }
 
     function destroy() {
-      $('[data-star]', $el).each(function () {
-        $(this).off('click', onClick);
+      $("[data-star]", $el).each(function () {
+        $(this).off("click", onClick);
       });
 
       $el.remove();
     }
 
-    $('[data-star]', $el).each(function () {
-      $(this).on('click', onClick);
+    $("[data-star]", $el).each(function () {
+      $(this).on("click", onClick);
     });
   }
 
-  $('.kk-star-ratings').each(function () {
+  $(".kk-star-ratings").each(function () {
     apply($(this));
   });
 });
