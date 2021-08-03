03080f4c3c385e4ef4711eed979182984525da86
Kamal Khan <shout@bhittani.com>
2021-08-03 14:14:11 +0500

    Add admin asset hooks

---
+++ src/wp/actions/admin_enqueue_scripts.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of bhittani/kk-star-ratings.
+ *
+ * (c) Kamal Khan <shout@bhittani.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Bhittani\StarRating\wp\actions;
+
+use function Bhittani\StarRating\functions\action;
+
+if (! defined('KK_STAR_RATINGS')) {
+    http_response_code(404);
+    exit();
+}
+
+function admin_enqueue_scripts($hook): void
+{
+    if ($hook == ('toplevel_page_'.kksr('slug'))) {
+        action('admin/style');
+        action('admin/script');
+    }
+}
