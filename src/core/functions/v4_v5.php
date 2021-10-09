ac22643171e4d5ed11f7dc2bf805829a78a0cc26
Kamal Khan <shout@bhittani.com>
2021-10-09 17:28:26 +0500

    Fix upgradation

--- src/core/functions/v4_v5.php
+++ src/core/functions/v4_v5.php
@@ -20,13 +20,15 @@ function v4_v5(): void
 {
     // Upgrade options
 
-    $varRegex = '/\[([\s\S]*?)\]/';
+    $varRegex = '/\[([a-zA-Z0-9_-]+?)\]/';
     $varReplacement = '{$1}';
 
-    option([
-        'legend' => preg_replace($varRegex, $varReplacement, get_option('kksr_legend')),
+    option(array_filter([
+        'greet' => preg_replace($varRegex, $varReplacement, get_option('kksr_greet')),
         'sd' => preg_replace($varRegex, $varReplacement, get_option('kksr_sd')),
-    ]);
+         // This was used in v2 but not afterwards.
+        'legend' => preg_replace($varRegex, $varReplacement, option('legend')),
+    ]));
 
     // Upgrade posts
 
@@ -34,18 +36,15 @@ function v4_v5(): void
 
     $rows = $wpdb->get_results("
         SELECT
-            posts.ID,
-            postmeta_ratings.meta_value as ratings,
-            postmeta_casts.meta_value as casts,
-            postmeta_status.meta_value as status
+            posts.ID
+        ,   postmeta_ratings.meta_value as ratings
+        ,   postmeta_casts.meta_value as casts
         FROM {$wpdb->posts} posts
         JOIN {$wpdb->postmeta} postmeta_ratings ON posts.ID = postmeta_ratings.post_id
         JOIN {$wpdb->postmeta} postmeta_casts ON posts.ID = postmeta_casts.post_id
-        JOIN {$wpdb->postmeta} postmeta_status ON posts.ID = postmeta_status.post_id
         WHERE
             postmeta_ratings.meta_key = '_kksr_ratings'
         AND postmeta_casts.meta_key = '_kksr_casts'
-        AND postmeta_status.meta_key = '_kksr_status'
     ");
 
     foreach ($rows as $row) {
@@ -53,7 +52,7 @@ function v4_v5(): void
             'count_default' => $count = (int) $row->casts,
             'ratings_default' => $ratings = (float) $row->ratings,
             'avg_default' => $count ? ($ratings / $count) : 0,
-            'status_default' => $row->status,
+            'status_default' => get_post_meta($row->ID, '_kksr_status', true),
         ]);
 
         foreach (get_post_meta($row->ID, '_kksr_ref', false) as $fingerprint) {
