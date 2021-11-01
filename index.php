f1344e9cff08a5712d18c20eb9376b88221787fc
Kamal Khan <shout@bhittani.com>
2021-11-01 19:30:55 +0500

    Prepare version 5.1.1

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
  * Author URI:      http://bhittani.com
  * Text Domain:     kk-star-ratings
  * Domain Path:     /languages
- * Version:         5.1.0
+ * Version:         5.1.1
  * License:         GPLv2 or later
  */
 
