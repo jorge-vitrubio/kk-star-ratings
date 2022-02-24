40b9abc5b56d8fc9ab5ff52bbdbf634ac163f4d4
Kamal Khan <shout@bhittani.com>
2022-02-24 18:34:44 +0500

    Tested upto 5.9.1

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
  * Author URI:      http://bhittani.com
  * Text Domain:     kk-star-ratings
  * Domain Path:     /languages
- * Version:         5.2.8
+ * Version:         5.2.9
  * License:         GPLv2 or later
  */
 
