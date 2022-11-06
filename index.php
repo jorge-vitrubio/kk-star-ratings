48f3107fe8dcf055eea689c44875ab565215055d
Kamal Khan <shout@bhittani.com>
2022-11-06 16:13:41 +0500

    Remove spaces

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
 <?php
 
-/**
- * Plugin Name:     kk Star Ratings
- * Plugin Slug:     kk-star-ratings
- * Plugin Nick:     kksr
- * Plugin URI:      https://github.com/kamalkhan/kk-star-ratings
- * Description:     Allow blog visitors to involve and interact more effectively with your website by rating posts.
- * Author:          Kamal Khan
- * Author URI:      http://bhittani.com
- * Text Domain:     kk-star-ratings
- * Domain Path:     /languages
- * Version:         5.3.4
- * License:         GPLv2 or later
+/*
+ * This file is part of bhittani/kk-star-ratings.
+ *
+ * (c) Kamal Khan <shout@bhittani.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
  */
 
 use function Bhittani\StarRating\core\functions\action;
@@ -30,7 +25,7 @@ require_once __DIR__.'/vendor/autoload.php';
 if (function_exists('kksr_freemius')) {
     kksr_freemius()->set_basename(true, __FILE__);
 } else {
-    if (! function_exists( 'kksr_freemius' )) {
+    if (! function_exists('kksr_freemius')) {
         require_once __DIR__.'/freemius.php';
     }
 
