fdc851deb882c04fe78c9d7607b7192ff9e6b712
Kamal Khan <shout@bhittani.com>
2023-10-21 23:29:08 +0500

    Lock ratings for 5 seconds to avoid race condition

--- src/core/wp/actions/wp_ajax_kk-star-ratings.php
+++ src/core/wp/actions/wp_ajax_kk-star-ratings.php
@@ -13,6 +13,8 @@ namespace Bhittani\StarRating\core\wp\actions;
 
 use function Bhittani\StarRating\core\functions\action;
 use function Bhittani\StarRating\core\functions\filter;
+use function Bhittani\StarRating\core\functions\lock;
+use function Bhittani\StarRating\core\functions\option;
 use function Bhittani\StarRating\functions\cast;
 use function Bhittani\StarRating\functions\sanitize;
 use function Bhittani\StarRating\functions\to_shortcode;
@@ -23,38 +25,56 @@ if (! defined('KK_STAR_RATINGS')) {
     exit();
 }
 
+class WpAjaxKkStarRatingsRaceException extends Exception
+{
+    public function __construct(?string $message = null, int $code = 403)
+    {
+        $message = $message ?: __('Please try again later.', 'kk-star-ratings');
+        parent::__construct($message, $code);
+    }
+}
+
 function wp_ajax_kk_star_ratings()
 {
+    $payload = sanitize($_POST['payload'] ?? []);
+    $payload['id'] = intval($payload['id'] ?? 0);
+    $payload['slug'] = $payload['slug'] ?? 'default';
+    $payload['best'] = intval($payload['best'] ?? option('stars'));
+    $payload['_legend'] = $payload['_legend'] ?? option('legend');
+    $rating = intval($_POST['rating'] ?? 0);
+
+    $lock = lock(__FUNCTION__."_{$payload['id']}_{$payload['slug']}", 5);
+
     try {
         if (! check_ajax_referer(__FUNCTION__, 'nonce', false)) {
             throw new Exception(__('This action is forbidden.', 'kk-star-ratings'), 403);
         }
 
-        $payload = sanitize($_POST['payload'] ?? []);
-
-        $id = intval($payload['id'] ?? 0);
-        $slug = $payload['slug'] ?? 'default';
-        $best = intval($payload['best'] ?? 5);
+        try {
+            $lock->acquire();
+        } catch (Exception $e) {
+            throw new WpAjaxKkStarRatingsRaceException();
+        }
 
         if (filter('validate', null, $payload['id'], $payload['slug'], $payload) === false) {
+            $lock->release();
             throw new Exception(__('A rating can not be accepted at the moment.', 'kk-star-ratings'));
         }
 
-        if (! isset($_POST['rating'])) {
+        if (! $rating) {
+            $lock->release();
             throw new Exception(__('A rating is required to cast a vote.', 'kk-star-ratings'));
         }
 
-        $rating = intval($_POST['rating'] ?? 0);
-
-        if ($rating < 1 || $rating > $best) {
-            throw new Exception(sprintf(__('The rating value must be between %1$d and %2$d.', 'kk-star-ratings'), 1, $best));
+        if ($rating < 1 || $rating > $payload['best']) {
+            throw new Exception(sprintf(__('The rating value must be between %1$d and %2$d.', 'kk-star-ratings'), 1, $payload['best']));
         }
 
-        $outOf5 = cast($rating, 5, $best);
+        $outOf5 = cast($rating, 5, $payload['best']);
 
-        action('save', $outOf5, $id, $slug, [
-            'count' => (int) filter('count', null, $id, $slug),
-            'ratings' => (float) filter('ratings', null, $id, $slug),
+        action('save', $outOf5, $payload['id'], $payload['slug'], [
+            'count' => (int) filter('count', null, $payload['id'], $payload['slug']),
+            'ratings' => (float) filter('ratings', null, $payload['id'], $payload['slug']),
         ] + $payload);
 
         $payload['legend'] = $payload['_legend'];
@@ -65,8 +85,12 @@ function wp_ajax_kk_star_ratings()
 
         wp_die($html, 201);
     } catch (Exception $e) {
+        // $lock->release();
+
         header('Content-Type: application/json; charset=utf-8');
 
         wp_die(json_encode(['error' => $e->getMessage()]), $e->getCode() ?: 406);
+    } catch (WpAjaxKkStarRatingsRaceException $e) {
+        wp_die(json_encode(['error' => $e->getMessage()]), $e->getCode() ?: 403);
     }
 }
